90533b66305c08f68dd79fc57f6a408365958cf1
mianawais12615 <mianawais12615@gmail.com>
2026-04-06 01:11:05 +0500

    u4

--- Controllers/SaveURL.js
+++ Controllers/SaveURL.js
@@ -24,7 +24,7 @@ export const SaveURL = async (req, res) => {
     const shortId = generateShortId(7);
     const newURL = new URLs({ longUrl: longUrl, shortId: shortId });
     await newURL.save();
-    const shortURL = `http://localhost:5050/${shortId}`;
+    const shortURL = `https://tinyurl2.up.railway.app/${shortId}`;
     
     console.log(`URL shortened: ${longUrl} -> ${shortURL}`);
     
